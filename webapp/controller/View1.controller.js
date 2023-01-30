ef5ff77f3400b030a569f95999bd5b204f2257f2
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-01-30 12:52:06 +0530

    Updated View and Controll

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -4,6 +4,12 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("GithubSAP-UI5-Fiori-8_XMLModel.controller.View1", {
+		
+		onInit : function(){
+			var oModel = new sap.ui.model.xml.XMLModel();
+			oModel.loadData("model/student.xml");
+			this.getView().setModel(oModel);
+		}
 
 	});
 });
